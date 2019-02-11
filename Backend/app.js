3a9c155ad01531f3f0e3929130ed9a544acf1643
Leonel Elanga <leonelelanga@yahoo.fr>
2019-02-11 21:30:06 +0100

    amelioration socle

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,9 @@
 var express = require('express');
+const http = require('http');
 var app = express();
 var UserController = require('./User/UserController');
 var bodyParser = require('body-parser');
+var path = require('path');
 var cors = require('cors');
 
 app.options('*', cors());
@@ -11,9 +13,14 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-
+app.use(express.static(path.join(__dirname, '/dist/Frontend')));
 app.use(bodyParser.json());
 app.use('/api-user', UserController);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/dist/Frontend/index.html'));
+});
+
 app.use(bodyParser.urlencoded({
     extended: true
 }));
