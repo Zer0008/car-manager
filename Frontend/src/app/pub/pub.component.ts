47a8a92aa5e4f71a172219a0d571ac05aac89b19
Arnaud NDIO <arnaud.ndioazom@gmail.com>
2019-03-23 21:36:36 +0100

    Component Pub

--- Frontend/src/app/pub/pub.component.ts
+++ Frontend/src/app/pub/pub.component.ts
@@ -7,22 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PubComponent implements OnInit {
 
-  listPubs = ["/assets/gifPub/renaultPub.gif","../assets/gifPub/tlchargement-2.jpeg","../assets/gifPub/parfum.jpg"];
-
-  id: number;
+  listPubs = [
+      {
+          "root": "/assets/gifPub/renaultPub.gif",
+          "site": "https://www.renault.fr"
+      },
+      {
+          "root": "../assets/gifPub/tlchargement-2.jpeg",
+          "site": "https://www.unibet.fr"
+      },
+      {
+          "root": "../assets/gifPub/parfum.jpg",
+          "site": "https://www.sephora.fr"
+      },
+      {
+          "root": "../assets/gifPub/bouygues.jpg",
+          "site": "https://www.bouyguestelecom.fr"
+      }
+      ];
 
   rand: number;
   image: string;
+  site: string;
 
   //rand = Math.round(Math.random()*(this.listPubs.length-1));
   //image = this.listPubs[this.rand];
 
   constructor() {
+
+    this.image = this.listPubs[Math.round(Math.random()*(this.listPubs.length-1))].root;
+
     setInterval(
         () => {
             this.rand = Math.round(Math.random()*(this.listPubs.length-1));
-            this.image = this.listPubs[this.rand];
-            console.log(this.listPubs[this.rand]);
+            this.image = this.listPubs[this.rand].root;
+            this.site = this.listPubs[this.rand].site;
+            //console.log(this.image);
+            //console.log(this.site);
         }, 5000
     );
   }
