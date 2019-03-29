0cb736e1e55fe6e2862eb3956cdf2784f5ef22bf
Leonel Elanga <leonelelanga@yahoo.fr>
2019-03-29 03:39:00 +0100

    finalisation du projet

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -19,12 +19,14 @@ import { CarListComponent } from './car-list/car-list.component';
 import { CarCreationComponent } from './car-creation/car-creation.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { CarViewComponent } from './car-view/car-view.component';
+import {  FileUploadModule } from 'ng2-file-upload';
 import { ViewUserComponent } from './view-user/view-user.component';
 import { PdfViewerModule } from 'ng2-pdf-viewer';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import { AnnonceVenteCreationComponent } from './annonce-vente-creation/annonce-vente-creation.component';
 import { AnnonceInterventionCreationComponent } from './annonce-intervention-creation/annonce-intervention-creation.component';
 import { VenteVehiculeComponent } from './vente-vehicule/vente-vehicule.component';
+import { ImageViewerModule } from 'ng-image-viewer';
 
 @NgModule({
   declarations: [
@@ -58,6 +60,8 @@ import { VenteVehiculeComponent } from './vente-vehicule/vente-vehicule.componen
     MatRadioModule,
     PdfViewerModule,
     MatProgressSpinnerModule,
+    ImageViewerModule,
+    FileUploadModule
   ],
   providers: [],
   bootstrap: [AppComponent]
